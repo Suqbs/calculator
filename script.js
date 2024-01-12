5665ea5d675df5975911a47b1fb04fa09c943d4b
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-01-12 16:50:54 +0300

    Resolved distinc number & operators

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
-function operate() {
+// Variable to store the display value
+let displayField = document.getElementById("result");
+displayField.value = 0;
+const calc = new operate();
+
+function operate(displayValue) {
+  const number1 = displayValue;
+  const number2 = 0;
+
+  console.log(this);
+
   this.methods = {
     "-": (a, b) => a - b,
     "+": (a, b) => a + b,
     "*": (a, b) => a * b,
     "/": (a, b) => a / b,
+    C: () => {},
   };
+  // if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+  //   console.log(this);
+  //   return NaN;
+  // }
 
-  this.calculate = function () {
-    let split = str.split(" "),
-      a = +split[0],
-      op = split[1],
-      b = +split[2];
-
-    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-      return NaN;
-    }
-
-    return this.methods[op](a, b);
-  };
+  // return this.methods[op](a, b);
 }
-
-const calc = new operate();
-
-// Variable to store the display value
-let displayValue = "";
 
 // Function to handle clicks on number buttons
 function handleNumberClick(number) {
-  displayValue += number;
-  console.log(displayValue);
-  // Update the display element
-  document.getElementById("result").value = displayValue;
+  // If the current value is 0, replace it with the number
+  if (displayField.value == "0") {
+    displayField.value = number;
+  } else 
+  {
+    // Otherwise, append the number
+    displayField.value += number;
+  }
 }
 
-const numberButtons = document.querySelectorAll("td input[type='button']");
+function handleOperatorClick(button) {
+  console.log("oldu");
+}
 
-numberButtons.forEach(button => {
-  if(isNumber(+button.value)) {
+const allButtons = document.querySelectorAll("td input[type='button']");
+
+allButtons.forEach((button) => {
+  if (isNumber(+button.value)) {
     console.log(button);
     button.addEventListener("click", () => handleNumberClick(button.value));
   }
+
+  if (button.value.search(/[+\-*/=C]/) != -1)
+    button.addEventListener("click", () => handleOperatorClick(button.value));
 });
+
+allButtons.forEach(button => button.addEventListener("click", () => console.log(button)));
 
 function isNumber(value) {
   return !isNaN(value); // Check if the value is not NaN (Not a Number)
 }
 
-
-
+console.log(calc);
