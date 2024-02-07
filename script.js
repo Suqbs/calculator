779ba5a49e3e0822a7870869d553e42498414028
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-02-07 18:25:38 +0300

    Add clicked numbers to screen

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const calculator = document.querySelector(".innerDiv");
+const display = document.getElementById("result");
 const keys = calculator.querySelectorAll("td input[type='button']");
 
 keys.forEach((key) => {
   key.addEventListener("click", (e) => {
     const clickedKey = e.target;
-    console.log(clickedKey);
     const action = key.dataset.action;
+    const keyValue = clickedKey.value;
+    const displayedNum = display.value;
+    console.log(keyValue);
+    console.log(displayedNum);
+
+    if (!action) {
+      if (displayedNum === "0") {
+        display.value = keyValue;
+      } else {
+        display.value = displayedNum + keyValue;
+      }
+    }
 
     switch (action) {
       case "add":
@@ -16,15 +28,13 @@ keys.forEach((key) => {
         break;
       case "decimal":
         console.log("decimal key!");
+        display.value = displayedNum + ".";
         break;
       case "clear":
         console.log("clear key!");
         break;
       case "calculate":
         console.log("calculate key!");
-        break;
-      default:
-        console.log("number key");
         break;
     }
     // if (
