04f109856cc5f6cd355f8862abe9f93471deddb5
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-02-07 13:13:13 +0300

    Take data-action from HTML

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const calculator = document.querySelector('innerDiv');
-const keys =  calculator.querySelector("td input[type='button']");
+const calculator = document.querySelector(".innerDiv");
+const keys = calculator.querySelectorAll("td input[type='button']");
 console.log(calculator);
 console.log(keys);
+
+keys.forEach((key) => {
+  key.addEventListener("click", (e) => {
+    const clickedKey = e.target;
+    console.log(clickedKey);
+    const action = key.dataset.action;
+    console.log(typeof action);
+    console.log(action);
+    if (!action) {
+      console.log("number key");
+    }
+
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator key!");
+    }
+  });
+});
