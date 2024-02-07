eb13f50b7470f6ffa05aa69287443a9ddd9500b8
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-02-07 16:03:11 +0300

    Change if conditions to switch

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const calculator = document.querySelector(".innerDiv");
 const keys = calculator.querySelectorAll("td input[type='button']");
-console.log(calculator);
-console.log(keys);
 
 keys.forEach((key) => {
   key.addEventListener("click", (e) => {
     const clickedKey = e.target;
     console.log(clickedKey);
     const action = key.dataset.action;
-    console.log(typeof action);
-    console.log(action);
-    if (!action) {
-      console.log("number key");
-    }
 
-    if (
-      action === "add" ||
-      action === "subtract" ||
-      action === "multiply" ||
-      action === "divide"
-    ) {
-      console.log("operator key!");
+    switch (action) {
+      case "add":
+      case "subtract":
+      case "multiply":
+      case "divide":
+        console.log("operator key");
+        break;
+      case "decimal":
+        console.log("decimal key!");
+        break;
+      case "clear":
+        console.log("clear key!");
+        break;
+      case "calculate":
+        console.log("calculate key!");
+        break;
+      default:
+        console.log("number key");
+        break;
     }
+    // if (
+    //   action === "add" ||
+    //   action === "subtract" ||
+    //   action === "multiply" ||
+    //   action === "divide"
+    // ) {
+    //   console.log("operator key!");
+    // }
   });
 });
